c3d7a06e4ed229438769d9425c5522f6894fb7be
AveryVi <main.averyvi@gmail.com>
2025-09-15 13:27:50 +0200

    made it compilable

---
+++ app/src/main/java/com/averyvi/bilbo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.averyvi.bilbo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.averyvi.bilbo.appuis.AppUI
+import com.averyvi.bilbo.ui.theme.BilboTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BilboTheme {
+                AppUI()
+            }
+        }
+    }
+}
