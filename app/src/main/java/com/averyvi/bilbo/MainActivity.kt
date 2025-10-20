67e8df01daf98c02b3aa909c1b4059316b6fb0c9
AveryVi <main.averyvi@gmail.com>
2025-10-20 12:30:40 +0200

    started work on bt

--- app/src/main/java/com/averyvi/bilbo/MainActivity.kt
+++ app/src/main/java/com/averyvi/bilbo/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.averyvi.bilbo
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
 import com.averyvi.bilbo.ui.theme.BilboTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +18,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BilboTheme {
+                val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+                val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+
+                if (bluetoothAdapter?.isEnabled == false) {
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                            1
+                        )
+
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return
+                    }
+                }
+
                 AppUI()
             }
         }
