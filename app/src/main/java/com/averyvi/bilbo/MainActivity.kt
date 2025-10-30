d1b903df10107153085601e93cad246174485dd5
AveryVi <main.averyvi@gmail.com>
2025-10-30 21:53:10 +0100

    I managed to get permissions

--- app/src/main/java/com/averyvi/bilbo/MainActivity.kt
+++ app/src/main/java/com/averyvi/bilbo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.averyvi.bilbo
 
 import android.Manifest
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -17,45 +18,145 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.averyvi.bilbo.ui.theme.BilboTheme
 import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity() {
+    private lateinit var bluetoothManager: BluetoothManager
+    private var bluetoothAdapter: BluetoothAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        bluetoothManager = getSystemService(BluetoothManager::class.java)
+        bluetoothAdapter = bluetoothManager.adapter
+
         enableEdgeToEdge()
+        beginBluetooth()
         setContent {
             BilboTheme {
-                // check bluetooth avalibility
-                val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
-                val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-
-                // check if the device has bt and if it is enabled
-                val permissionsNeeded = arrayOf(
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                )
-
-                if (bluetoothAdapter?.isEnabled == false) {
-                    permissionsNeeded.forEach { permission ->
-                         if(ActivityCompat.checkSelfPermission(
-                                this,
-                                permission
-                            ) != PackageManager.PERMISSION_GRANTED
-                         ) {
-                             ActivityCompat.requestPermissions(
-                                 this,
-                                 arrayOf(permission),
-                                 1
-                             )
-                         }
-                    }
-                }
                 AppUI()
             }
         }
     }
+
+    /*permissions**********************************************************************************************/
+
+    fun checkEnablePermissions(): Boolean{
+        // check if the device has bt and if it is enabled
+        val permissionsNeeded = arrayOf(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+
+        val permissionsNotGranted = permissionsNeeded.filterNot { hasPermission(it) }
+
+        if(permissionsNotGranted.isEmpty()){
+            Log.d("Permissions", "All necessary permissions already granted.")
+            return true
+        } else{
+            Log.d("Permissions", "Requesting necessary permissions.")
+            requestPermissions.launch(permissionsNeeded)
+            return requestPermissionsReturn?:false
+        }
+    }
+
+    var requestPermissionsReturn: Boolean? = null
+    val requestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val allPermissionsGranted = permissions.all { it.value }
+            if (allPermissionsGranted) {
+                Log.d("Permissions", "All permissions granted.")
+                requestPermissionsReturn = true
+            } else {
+                Log.w("Permissions", "User denied one or more permissions. Cannot perform BLE operations.")
+                requestPermissionsReturn = true
+            }
+        }
+
+    var enablePermissionReturn: Boolean? = null
+    val enablePermission =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d("Permissions", "Bluetooth enabled by user.")
+                enablePermissionReturn = true
+
+            } else {
+                Log.d("Permissions", "User denied Bluetooth enabling.")
+                enablePermissionReturn = false
+            }
+        }
+
+    fun hasPermission(permission: String): Boolean {
+        val granted = ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        if (!granted) {
+            Log.w("Permissions", "Permission not granted: $permission")
+        }
+        return granted
+    }
+
+    /*begin bluetooth**********************************************************************************************/
+
+    fun beginBluetooth() {
+        Log.d("Permissions", "Checking for necessary permissions.")
+        if(checkEnablePermissions()){
+            proceedWithBluetooth()
+        } else{
+            Log.d("Permissions", "App does not have the permissions for bluetooth")
+        }
+    }
+
+    fun proceedWithBluetooth(){
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            enablePermission.launch(enableIntent)
+            if(enablePermissionReturn?:false){
+                Log.d("BluetoothBegin", "Scanning begins.")
+                //startBluetoothScan()
+            }
+        } else if (bluetoothAdapter != null) {
+            Log.d("BluetoothBegin", "Scanning begins.")
+            //startBluetoothScan()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     fun findDevice(){
         val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
