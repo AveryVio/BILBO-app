87736766b774309203f13e5446ce192972eca484
AveryVi <main.averyvi@gmail.com>
2026-03-13 20:25:43 +0100

    started work on permanent storage

--- app/src/main/java/com/averyvi/bilbo/storage/PermanentStorage.kt
+++ app/src/main/java/com/averyvi/bilbo/storage/PermanentStorage.kt
@@ -1,2 +1,20 @@
 package com.averyvi.bilbo.storage
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class InstrumentProfile(
+    val refFreq: Int,
+    val positionInOctive: Int,
+    val refOctive: Int
+)
+
+@Entity
+class InstrumentDBRow(
+    @PrimaryKey(autoGenerate = false) val instrumentName:String = "Piano",
+    @ColumnInfo(name = "InstrumentIcon") val instrumentIcon: Int = 0,
+    @ColumnInfo(name = "refFreq") val refFreq: Int = 0,
+    @ColumnInfo(name = "positionInOctive") val positionInOctive: Int = 0,
+    @ColumnInfo(name = "refOctive") val refOctive: Int = 0,
+)
